a6486a58359a9b65dda8a938f928a76343b44fe4
Vitor Pio <vitormarquespio@gmail.com>
2024-09-12 20:10:53 -0300

    ajustar logica de pulo

--- objects/player.gd
+++ objects/player.gd
@@ -2,10 +2,10 @@ extends RigidBody2D
 
 const WALK_ANIMATION = "walk"
 const IDLE_ANIMATION = "idle"
+const JUMP_ANIMATION = "jump"
 
 @export var speed = 400
-@export var jump_force = -500
-@export var is_jumping = false
+@export var jump_force = -600
 
 var velocity = Vector2.ZERO
 var screen_size
@@ -20,9 +20,8 @@ func _physics_process(_delta):
 			velocity.x += 1
 		if Input.is_action_pressed("move_left"):
 			velocity.x -= 1
-		if Input.is_action_just_released('jump'):
+		if Input.is_action_just_released('jump') and linear_velocity.y == 0:
 			self.apply_impulse(Vector2(0, jump_force))
-			is_jumping = true
 			
 		if velocity.length() > 0:
 			velocity = velocity.normalized() * speed
@@ -32,15 +31,15 @@ func _physics_process(_delta):
 		position += velocity * _delta
 			
 		# Update animation
-		if velocity.x != 0:
-			if !is_jumping:
-				$AnimatedSprite2D.animation = WALK_ANIMATION
-			else:
-				pass
+		if velocity.x != 0 and linear_velocity.y == 0:
+			$AnimatedSprite2D.animation = WALK_ANIMATION
+			$AnimatedSprite2D.flip_h = velocity.x < 0
+		elif linear_velocity.y != 0:
+			$AnimatedSprite2D.animation = JUMP_ANIMATION
 			$AnimatedSprite2D.flip_h = velocity.x < 0
 		else:
 			$AnimatedSprite2D.animation = IDLE_ANIMATION
 
-func _on_area_2d_body_entered(body):
-	if body is TileMap:
-		is_jumping = false
+#func _on_area_2d_body_entered(body):
+	#if body is TileMap:
+		#is_jumping = false
